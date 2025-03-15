3d1f4ba4712409c7da91c76d5913d55fd2107c25
Kristian Cross <etamin1985@yahoo.com>
2025-03-15 20:53:26 +0000

    Change settings module to core.settings

--- core/asgi.py
+++ core/asgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.asgi import get_asgi_application
 
 # Set the default settings module for the 'asgi' application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tax_ni_dashboard.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 # Get the ASGI application instance
 application = get_asgi_application()
--- core/wsgi.py
+++ core/wsgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Set the default settings module for the 'wsgi' application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tax_ni_dashboard.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 # Get the WSGI application instance
 application = get_wsgi_application()
