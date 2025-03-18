bbfbbdf75ca59a2f83dbce642ac59f545eed2be4
KC-85 <etamin1985@yahoo.com>
2025-03-18 22:22:26 +0000

    Move error handling urls to core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler400, handler403, handler404, handler500
+from dashboard import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Django Admin Panel
     path('', include('dashboard.urls')),  # Include dashboard app URLs
+
+    handler400 = "dashboard.views.error_400"
+    handler403 = "dashboard.views.error_403"
+    handler404 = "dashboard.views.error_404"
+    handler500 = "dashboard.views.error_500"
 ]
 
 # Serve static and media files during development
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,15 +1,8 @@
 from django.urls import path
 from . import views
-from django.conf.urls import handler400, handler403, handler404, handler500
-from dashboard import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),  # Homepage (Main Dashboard)
     path('income/', views.add_income, name='add_income'),  # Income Entry Page
     path('expenses/', views.add_expense, name='add_expense'),  # Expenses Entry Page
-   
-    handler400 = "dashboard.views.error_400"
-    handler403 = "dashboard.views.error_403"
-    handler404 = "dashboard.views.error_404"
-    handler500 = "dashboard.views.error_500"
 ]
